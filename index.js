98b8052ab93dc3c9b4560dbc05cf6808e2437172
Hrishabh Tiwari <rishabh2560@gmail.com>
2024-03-24 19:56:26 +0530

    deployment setup

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const path = require("path");
 const { OrderRoutes, ProductRoutes } = require("./routes");
 const connectDB = require("./utils/ConnectDB");
 const app = express();
@@ -10,9 +11,20 @@ const PORT = process.env.PORT || 8000;
 app.use(express.json({ limit: "50mb" }));
 app.use(bodyParser.json());
 app.use(cors());
+
 app.use("/api/orders", OrderRoutes);
 app.use("/api/product", ProductRoutes);
 
+app.get("/", (req, res) => {
+  console.log("working");
+  console.log(`client/build/${req.url !== "/" ? req.url : "index.html"}`);
+  res.sendFile(
+    path.join(
+      __dirname,
+      `client/build/${req.url !== "/" ? req.url : "index.html"}`
+    )
+  );
+});
 const startServer = async () => {
   try {
     connectDB(process.env.MONGODB_URL);
