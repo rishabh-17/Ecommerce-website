718f1d73df1bb8859b8c8c7ae40633f2057368ff
Hrishabh Tiwari <rishabh2560@gmail.com>
2024-03-24 20:01:07 +0530

    deployment ready

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ app.use(express.json({ limit: "50mb" }));
 app.use(bodyParser.json());
 app.use(cors());
 
+app.use(express.static(__dirname + "/client/build"));
+
 app.use("/api/orders", OrderRoutes);
 app.use("/api/product", ProductRoutes);
 
